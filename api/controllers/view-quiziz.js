12c7ceb472c3452b7f9f979802f8b81224d2a07e
Ashok <ashok.jaga@gmail.com>
2022-10-30 18:35:42 -0700

    Removed jsPDF references

--- api/controllers/view-quiziz.js
+++ api/controllers/view-quiziz.js
@@ -49,19 +49,6 @@ module.exports = {
 
   fn: async function (inputs,exits,env) 
   {
-    var doc = new jsPDF();  
-    var specialElementHandlers = {  
-    '#editor': function (element, renderer) {  
-        return true;  
-    }  
-    }; 
-    $('#submit').click(function () {  
-        doc.fromHTML($('#content').html(), 15, 15, {  
-            'width': 190,  
-                'elementHandlers': specialElementHandlers  
-        });  
-        doc.save('sample-page.pdf');  
-    }); 
     //-------------------------------------------------------------------------------
        var count= await Questions.count({category:inputs.category,});
     var randm = Math.floor((Math.random() * count));
