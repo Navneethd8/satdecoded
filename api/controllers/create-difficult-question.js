8bd4aa05bb8f60cf5ba5334ab7b385dd2287aea9
Navneeth <navneethd8@gmail.com>
2022-11-05 22:11:51 +0400

    begining difficult questions quiz part

---
+++ api/controllers/create-difficult-question.js
@@ -0,0 +1,85 @@
+module.exports = {
+
+
+  friendlyName: 'create difficult question',
+
+
+  description: '',
+
+
+  inputs: {
+          
+    answer1:
+    {
+      type: 'string',
+      required: false,
+    },
+    answer2:
+    {
+      type: 'string',
+      required: false,
+    },
+    answer3:
+    {
+      type: 'string',
+      required: false,
+    },
+    answer4:
+    {
+      type: 'string',
+      required: false,
+    },
+    correct_answer:
+    {
+      type: 'string',
+      required: false,
+    },
+    category:
+    {
+      type: 'string',
+      required: false,
+    },
+    qn:
+      {
+      type: 'string',
+      required: false,
+      },
+    answer_description:
+      {
+      type: 'string',
+      required: false,
+      },
+  
+
+
+  },
+
+
+  exits: {
+
+  },
+
+
+  fn: async function (inputs,exits,env) {
+
+    console.log("begin upload questions function")
+    console.log(inputs)
+     let difficult = await Difficult.create({
+       qn:inputs.qn,
+       category:inputs.category,
+       answer1:inputs.answer1,
+       answer2:inputs.answer2,
+       answer3:inputs.answer3,
+       answer4:inputs.answer4,
+       correct_answer:inputs.correct_answer,
+       answer_description:inputs.answer_description,
+       approved_status:
+      }).fetch();
+    return env.res.redirect("/admin_home_page.html")
+   
+
+
+  }
+
+
+};
