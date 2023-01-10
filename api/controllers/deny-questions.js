7ec3fa02ad03a25a3f2abf53685835671661ade5
Navneeth <navneethd8@gmail.com>
2023-01-10 11:41:42 +0400

    mroe frontend changes

---
+++ api/controllers/deny-questions.js
@@ -0,0 +1,68 @@
+// module.exports = {
+
+
+//   friendlyName: 'Deny questions',
+
+
+//   description: '',
+
+
+//   inputs: {
+
+//   },
+
+
+//   exits: {
+
+//   },
+
+
+//   fn: async function (inputs) {
+//     console.log("invoking deny")
+//     console.log("---Session---")
+//     console.log(env.req.cookies['user'])
+//     var user_id = 0
+//     if (env.req.cookies['user']) {
+//     const user = await User.findOne({ email: env.req.cookies['user']})
+//     console.log(user.email)
+//     email1=user.email
+//     console.log(user.id)
+//     user_id = user.id
+//     console.log(user_id)
+//     }
+//     console.log(inputs)
+//    try{
+//     console.log('entering deny function')
+//     const video = await Videos.findOne({ id: inputs.video_id });
+//       console.log(video);
+//       if (!video) {
+//         console.log("video not found")
+//         return env.res.redirect("/archives/error_somethingwentwrong.html")
+//      }
+//      console.log("set videos");
+//      await Videos.destroyOne({ id: video.id });
+//      console.log("Destroy Successful")
+     
+//   //  console.log(" before email=approved");
+//   //  console.log(inputs)
+//   //  const user = await User.findOne({ id: inputs.user_id});
+//   //   console.log("before email")
+//   //   console.log(email1)
+//   //    const email = {
+//   //     to: email1,
+//   //     subject: "Approval of videos",
+//   //     template: "vid_approved",
+      
+//   //    };
+//   //    console.log(email)
+//   //    await sails.helpers.sendMail(email);
+//   //     console.log("email approved sent ");
+//      return env.res.redirect("/list_videos")
+
+//     // All done.
+//     return;
+
+//   }
+
+
+// };
